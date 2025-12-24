b965286e069061faa35baeaeb57547e0ecb6d241
Sock-account <bromo.exe2@gmail.com>
2025-12-24 15:58:47 -0500

    Part 3

--- Text Adventure/TextAdvent/src/Window.java
+++ Text Adventure/TextAdvent/src/Window.java
@@ -4,9 +4,21 @@ public class Window extends JFrame {
     
     @SuppressWarnings("derprecation")
     public Window(){
-
+        JPanel window = new JPanel();
         JPanel Start = new JPanel();
+        JPanel Game = new JPanel();
 
-        String Title = "Text Adventure";
+        JLabel Title = new JLabel("Text Adventure");
+
+        Start.add(Title);
+        window.add(Start);
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().add(window);
+        window.setLayout(new FlowLayout());
+        this.setSize(new Dimension(500,500));
+        this.setVisible(true);
+        this.setTitle("Text Adventure");
     }
 }
