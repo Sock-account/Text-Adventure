1aa934d220dd9f98eeff50b6465a49861d60cac0
Sock-account <bromo.exe2@gmail.com>
2026-06-02 21:24:47 -0400

    The start button now changes the screen

--- Text Adventure/TextAdvent/src/Window.java
+++ Text Adventure/TextAdvent/src/Window.java
@@ -4,30 +4,35 @@ public class Window extends JFrame {
     
     //TODO:Figure out how to change the elements in the window dynamically 
     @SuppressWarnings("deprecation")
-    // Creates the game window for the game
+    // Creates the GUI for the game
     public Window(){
         JPanel window = new JPanel();
-        JPanel Start = new JPanel();
+        JPanel start = new JPanel();
         JPanel Game = new JPanel();
-
+        
         JLabel Title = new JLabel("The Game of Life");
 
-        Start.add(Title);
-        window.add(Start);
+        start.add(Title);
+        window.add(start);
+        
 
+        // Sets up the logic for the start screen
         JPanel start_button_space = new JPanel();
-
-
-
         JButton sb = new JButton();
-
-
-
         JLabel l1 = new JLabel("Start");
-
+        sb.add(l1);
         start_button_space.add(sb);
-        Start.add(start_button_space);
-        window.add(Start);
+        start.add(start_button_space);
+        sb.addActionListener(q ->{
+            start.remove(start_button_space);
+            start.remove(Title);
+            repaint();
+            Chara_Creator();
+        });
+
+
+
+
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().add(window);
@@ -35,5 +40,9 @@ public class Window extends JFrame {
         this.setSize(new Dimension(500,500));
         this.setVisible(true);
         this.setTitle("Text Adventure");
+    }
+
+    public void Chara_Creator(){
+        
     }
 }
