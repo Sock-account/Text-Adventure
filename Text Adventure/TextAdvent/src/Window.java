5581ab2230c25daf5db1a79a235701fe21e7133b
Sock-account <bromo.exe2@gmail.com>
2026-06-13 17:19:08 -0400

    I think that is all of the buttons

--- Text Adventure/TextAdvent/src/Window.java
+++ Text Adventure/TextAdvent/src/Window.java
@@ -72,58 +72,69 @@ public class Window extends JFrame {
         JLabel strength_num = new JLabel("0");
         JButton strength_b1 = new JButton("<");
         JButton strength_b2 = new JButton(">");
-
         strength_space.add(strength_label);
         strength_space.add(strength_b1);
         strength_space.add(strength_num);
         strength_space.add(strength_b2);
-
         chara_creator.add(strength_space);
+        
         // Intelligence panel
         JPanel int_space = new JPanel();
         JLabel int_label = new JLabel("Intelligence");
         JLabel int_num = new JLabel("0");
         JButton int_b1 = new JButton("<");
         JButton int_b2 = new JButton(">");
-
         int_space.add(int_label);
         int_space.add(int_b1);
         int_space.add(int_num);
         int_space.add(int_b2);
         chara_creator.add(int_space);
+        
         //Charisma panel
         JPanel charisma_space = new JPanel();
         JLabel charisma_label = new JLabel("Charisma");
         JLabel charisma_num = new JLabel("0");
         JButton charisma_b1 = new JButton("<");
         JButton charisma_b2 = new JButton(">");
-
         charisma_space.add(charisma_label);
         charisma_space.add(charisma_b1);
         charisma_space.add(charisma_num);
         charisma_space.add(charisma_b2);
         chara_creator.add(charisma_space);
+
         //Coordination panel
         JPanel coordination_space = new JPanel();
         JLabel coordination_label = new JLabel("Coordination");
         JLabel coordination_num = new JLabel("0");
         JButton coordination_b1 = new JButton("<");
         JButton coordination_b2 = new JButton(">");
-
         coordination_space.add(coordination_label);
         coordination_space.add(coordination_b1);
         coordination_space.add(coordination_num);
         coordination_space.add(coordination_b2);
         chara_creator.add(coordination_space);
+        
         //Quirks panel
         JPanel quirk_panel = new JPanel();
-        JButton quirk_b1 = new JButton("Asmatic");
-        JButton quirk_b2 = new JButton("Autistic");
-        
+        JLabel quirk_name = new JLabel("Quirks");
+        JButton quirk_b1 = new JButton("");
+        JButton quirk_b2 = new JButton("");
+        JButton quirk_b3 = new JButton("");
+        JButton quirk_b4 = new JButton("");
+        JButton quirk_b5 = new JButton("");
+        JButton quirk_b6 = new JButton("");
+        quirk_panel.add(quirk_name);
+        quirk_panel.add(quirk_b1);
+        quirk_panel.add(quirk_b2);
+        quirk_panel.add(quirk_b3);
+        quirk_panel.add(quirk_b4);
+        quirk_panel.add(quirk_b5);
+        quirk_panel.add(quirk_b6);
+        chara_creator.add(quirk_panel);
         window.add(chara_creator);
         window.revalidate();
         window.repaint();
 
-        chara_creator.setLayout(new FlowLayout());
+        chara_creator.setLayout(new GridLayout(6, 3));
     }
 }
